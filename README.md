0ee6886fdd628bbacba4f5d7685a73df1eff8999
PrekinII <prekinivan@gmail.com>
2023-09-02 17:43:26 +0500

    Task 2 commit

---
+++ README.md
@@ -0,0 +1,2 @@
+1. Задание №1 в Initial commit
+2. задание №2 в Task 2 commit
--- students_and_mentors.py
+++ students_and_mentors.py
@@ -7,14 +7,39 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def lecturer_hw(self, lecturer, course, grade):
+        if (isinstance(lecturer, Lecturer) and course in lecturer.courses_attached
+                and course in self.courses_in_progress and grade <= 10):
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+        # self.grades = {}
+
+
+class Lecturer(Mentor):
+
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
 
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if (isinstance(student, Student) and course in self.courses_attached
+                and course in student.courses_in_progress):
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
@@ -22,21 +47,29 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-    pass
-
-class Reviewer(Mentor):
-    pass
-
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+
 
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer = Reviewer('Alexandr', 'Bardin')
+cool_reviewer.courses_attached += ['Python']
+
+cool_lecturer = Lecturer('Elena', 'Nikitina')
+cool_lecturer.courses_attached += ['Python']
+
+best_student.lecturer_hw(cool_lecturer, 'Python', 5)
+best_student.lecturer_hw(cool_reviewer, 'Python', 10)
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
+
+print(cool_lecturer.__dict__)
+print(cool_reviewer.__dict__)
+
+print(cool_lecturer.grades)
+
 
