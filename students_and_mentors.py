6f373f2323240a2f0db9c9e1062825089f7ca15f
PrekinII <prekinivan@gmail.com>
2023-09-03 11:08:05 +0500

    Task 4 commit

--- students_and_mentors.py
+++ students_and_mentors.py
@@ -20,7 +20,7 @@ class Student:
     def middle_grade(self):  # Подсчет среднего балла
         m_grade = 0
         for grade in self.grades.values():
-            m_grade += sum(grade) / 2
+            m_grade += sum(grade) / len(grade) / len(self.grades)
         return m_grade
 
     def __str__(self):  # Переопределение __str_
@@ -53,7 +53,7 @@ class Lecturer(Mentor):
     def middle_grade(self):  # Подсчет среднего балла за лекции
         m_grade = 0
         for grade in self.grades.values():
-            m_grade += sum(grade) / 2
+            m_grade += sum(grade) / len(grade) / len(self.grades)
         return m_grade
 
     def __str__(self):  # Переопределение __str_
@@ -86,6 +86,7 @@ class Reviewer(Mentor):
         res = f'Имя: {self.name}\nФамилия: {self.surname}'
         return res
 
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 best_student.courses_in_progress += ['Java']
@@ -99,14 +100,20 @@ best_student2.finished_courses += ['Введение в программиров
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
-cool_mentor = Mentor('Some 2', 'Buddy 2')
-cool_mentor.courses_attached += ['Java']
+cool_mentor2 = Mentor('Some 2', 'Buddy 2')
+cool_mentor2.courses_attached += ['Java']
 
 cool_reviewer = Reviewer('Alexandr', 'Bardin')
 cool_reviewer.courses_attached += ['Python']
+cool_reviewer.courses_attached += ['Java']
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Java', 5)
+cool_reviewer.rate_hw(best_student2, 'Python', 9)
 
 cool_reviewer2 = Reviewer('Some_reviewer', 'Buddy_reviewer')
 cool_reviewer2.courses_attached += ['Java']
+cool_reviewer2.courses_attached += ['Python']
+cool_reviewer2.rate_hw(best_student2, 'Python', 9)
 
 cool_lecturer = Lecturer('Elena', 'Nikitina')
 cool_lecturer.courses_attached += ['Python']
@@ -126,19 +133,39 @@ best_student2.lecturer_hw(cool_lecturer2, 'Python', 10)
 best_student.lecturer_hw(cool_lecturer2, 'Java', 9)
 best_student2.lecturer_hw(cool_lecturer2, 'Java', 9)
 
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student2, 'Python', 9)
+all_students = [best_student, best_student2]  # Создаем список студентов и лекторов
+all_lecturers = [cool_lecturer, cool_lecturer2]
+
+def all_middle_grades(all_pers): # Создаем функцию для расчета среднего общего балла
+    all_m_grade = 0
+    for pers in all_pers:
+        all_m_grade += pers.middle_grade()
+    print(f'Общий средний балл: {all_m_grade / len(all_pers)}')
+    return
+
+all_middle_grades(all_students)
+all_middle_grades(all_lecturers)
 
 print(best_student.grades)
 print(best_student2.grades)
 
-print(cool_lecturer.__dict__)
-print(cool_reviewer.__dict__)
-
 print(cool_lecturer.grades)
+print(len(cool_lecturer.grades.values()))
+print(cool_lecturer2.grades)
 
 print(cool_reviewer)
 print(cool_lecturer)
+print(cool_lecturer2)
+print(best_student)
+print(best_student2)
+
+print(best_student > best_student2)
+
+print(cool_lecturer > cool_lecturer2)
+
+print(cool_reviewer)
+print(cool_lecturer)
+print(cool_lecturer2)
 print(best_student)
 print(best_student2)
 
